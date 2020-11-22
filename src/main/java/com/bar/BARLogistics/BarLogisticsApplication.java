abb2f218f0389f692d06516c3d51ad96eb5890a6
Rado Popov <altnum16@gmail.com>
2020-11-22 19:20:15 +0200

    Created project base

---
+++ src/main/java/com/bar/BARLogistics/BarLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.bar.BARLogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarLogisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bar/BARLogistics/BarLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bar.BARLogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarLogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
