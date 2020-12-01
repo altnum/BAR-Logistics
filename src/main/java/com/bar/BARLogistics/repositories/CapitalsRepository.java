e3fd4ce94c1c15626604c5f4a5390fb5bf81f839
Angelina Dachkinova <a.dachkinova@gmail.com>
2020-12-02 00:43:50 +0200

    added entities

---
+++ src/main/java/com/bar/BARLogistics/repositories/CapitalsRepository.java
@@ -0,0 +1,7 @@
+package com.bar.BARLogistics.repositories;
+
+import com.bar.BARLogistics.entities.Capitals;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapitalsRepository extends JpaRepository<Capitals, Long> {
+}
